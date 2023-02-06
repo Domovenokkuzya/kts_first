a43375c3bf1c5a69f9f57267e9d69fd05bc05a0b
Theofrast <fplotnikovfff@hotmail.com>
2023-02-07 01:33:51 +0300

    Изменение pairs и product

--- tasks/pairs.py
+++ tasks/pairs.py
@@ -12,13 +12,10 @@ def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any
     """
     if not arr1 or not arr2:
         return list()
-    arr3: list(Any, Any)
-    arr4: list(Any, Any)
-    arr3[0] = arr1[0]
-    arr3[1] = arr2[0]
-    arr4[0] = arr1[1]
-    arr4[1] = arr2[1]
+    arr3: tuple = (arr1[0], arr2[0])
+    arr4: tuple = (arr1[1], arr2[1])
+    arr5: list[tuple] = [arr3, arr4]
 
-    return list[tuple(arr1, arr2)]
+    return arr5
 
     raise NotImplementedError
--- tasks/product.py
+++ tasks/product.py
@@ -10,4 +10,14 @@ def cartesian_product(arr1: list[Any], arr2: list[Any]) -> list:
     Должна возвращать все пары элементы двух массивов:
     cartesian_product([1, 2], [3, 4]) == [(1, 3), (1, 4), (2, 3), (2, 4)]
     """
+    if not arr1 or not arr2:
+        return list()
+    combinations_quantity: int = len(arr1) * len(arr2)
+    arr: list[tuple] = []
+    combination: tuple[Any] = ()
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            combination = (arr1[i], arr2[j])
+            arr.append(combination)
+    return arr
     raise NotImplementedError
