6634fe40846550b3f7909f1b1f1a32ef0602fabc
Theofrast <fplotnikovfff@hotmail.com>
2023-02-07 02:03:35 +0300

    Изменение text

--- tasks/text.py
+++ tasks/text.py
@@ -10,4 +10,27 @@ def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]
     В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
     Если такого слова нет - вернуть None
     """
+    result: tuple = (None, None)
+    word: str = ""
+    short: str = ""
+    long: str = ""
+    text += " "
+    for i in text:
+        if ("a" <= i <= "z") or ("а" <= i <= "я"):
+            word += i
+        else:
+            if short == "":
+                short = word
+            if len(word) < len(short) and word != "":
+                short = word
+            if len(word) > len(long):
+                long = word
+            word = ""
+
+    print(short, long)
+    if short != "":
+        result = (short, long)
+    return result
     raise NotImplementedError
+
+find_shortest_longest_word('hello\n\n\tsir')
