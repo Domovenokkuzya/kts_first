89042db386ed784a07ee6c07c20650b67ccf9582
Theofrast <fplotnikovfff@hotmail.com>
2023-02-06 23:12:38 +0300

    Изменение prime

--- tasks/prime.py
+++ tasks/prime.py
@@ -7,7 +7,7 @@ def is_prime(number: int) -> bool:
     """
     Функция должна вернуть True если число является простым, иначе - False
     """
-    if number % 2 == 0 or number % 3 == 0 or number == 1:
+    if (number > 3 and (number % 2 == 0 or number % 3 == 0)) or number == 0 or number == 1:
         return False
     else:
         return True
