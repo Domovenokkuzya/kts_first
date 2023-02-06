0d4c400940648d67f4523807d087874793593020
Theofrast <fplotnikovfff@hotmail.com>
2023-02-07 00:31:15 +0300

    Изменение pairs

--- tasks/pairs.py
+++ tasks/pairs.py
@@ -12,12 +12,12 @@ def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any
     """
     if not arr1 or not arr2:
         return list()
-    arr3: list[Any]
-    arr4: list[Any]
+    arr3: list(Any, Any)
+    arr4: list(Any, Any)
     arr3[0] = arr1[0]
-    arr3[1] = arr3[1]
-    arr4[0] = arr4[0]
-    arr4[1] = arr4[1]
+    arr3[1] = arr2[0]
+    arr4[0] = arr1[1]
+    arr4[1] = arr2[1]
 
     return list[tuple(arr1, arr2)]
 
