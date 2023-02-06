d92a913200e0962d72419e697c34bc392d510366
Theofrast <fplotnikovfff@hotmail.com>
2023-02-07 00:13:21 +0300

    Изменение pairs

--- tasks/pairs.py
+++ tasks/pairs.py
@@ -10,4 +10,9 @@ def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any
     Функция должна возвращать соответствующие элементы двух массивов:
     corresponding_pairs([1, 2], [3, 4]) == [(1, 3), (2, 4)]
     """
+    if not arr1 or not arr2:
+        return list[tuple]
+    arr3: list[Any]
+    arr4: list[Any]
+
     raise NotImplementedError
