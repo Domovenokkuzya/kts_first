b92b8ab0c28081780c410fc292772ee60c958547
Theofrast <fplotnikovfff@hotmail.com>
2023-02-07 00:21:47 +0300

    Изменение pairs

--- tasks/pairs.py
+++ tasks/pairs.py
@@ -11,8 +11,10 @@ def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any
     corresponding_pairs([1, 2], [3, 4]) == [(1, 3), (2, 4)]
     """
     if not arr1 or not arr2:
-        return list[tuple]
-    arr3: list[Any]
-    arr4: list[Any]
+        return list()
+    a: Any = arr1[0]
+    arr1[0] = arr2[1]
+    arr2[0] = a
+    return list[tuple(arr1, arr2)]
 
     raise NotImplementedError
